e6aea6ee1be59bdd41a1e0eaff62cd22065847f4
Fadjumah2 <eritageentcare@gmail.com>
2026-09-20 19:32:00 +0300

    Build Street Club Entebbe website: add script.js

---
+++ script.js
@@ -0,0 +1,1 @@
+document.getElementById("year").textContent=new Date().getFullYear();const menu=document.querySelector(".menu"),nav=document.querySelector(".nav nav");menu?.addEventListener("click",()=>{nav.classList.toggle("open")});document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener("click",()=>nav?.classList.remove("open")));
